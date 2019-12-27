05cd854d95a3ced0585fda8bb944d603f38eac1e
AuCson <aucson@outlook.com>
2019-12-27 15:51:32 +0800

    fixed slideshow

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -58,11 +58,5 @@ function showSlides(slideIndex) {
 
 $(document).ready(function() {
   initialize_slides();
-  var _leftheight = $("#div-intro").height();
-  var _rightheight = $("#div-slides").height();
-  if (_leftheight > _rightheight) {
-      $("#div-slides").height(_leftheight);
-  } else {
-      $("#div").height(_rightheight);
-  }
+
 });
